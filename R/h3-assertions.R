a535972be9cfa881e8a5b13b45db9d06e8df9d89
John Coene <jcoenep@gmail.com>
2019-09-25 09:30:21 +0200

    better abstraction on h3-assertions

--- R/h3-assertions.R
+++ R/h3-assertions.R
@@ -5,17 +5,22 @@
 #' @inheritParams h3_to_geo
 #' 
 #' @examples
-#' h3_is_valid("invalid")
+#' h3_is_hex("invalid")
+#' h3_is_hex("809bfffffffffff")
+#' 
+#' geo_to_h3(quakes, lat, long, resolution = 3L) %>% 
+#'   h3_is_hex(hex = hex, .name = "is_it_a_valid_hex")
 #' 
 #' @return A vector or original data.frame as 
-#' \link[tibble]{tibble} with \code{is_valid}
-#' column.
+#' \link[tibble]{tibble} with an additional column 
+#' as specified by \code{.name} which defaults to 
+#' \code{is_hex}.
 #' 
 #' @export
-h3_is_valid <- function(data, ...) UseMethod("h3_is_valid")
+h3_is_hex <- function(data, ...) UseMethod("h3_is_hex")
 
 #' @export
-h3_is_valid.default <- function(data, ...){
+h3_is_hex.default <- function(data, ...){
   map(data, function(x){
     h3$call("h3.h3IsValid", x)
   }) %>% 
@@ -23,22 +28,93 @@ h3_is_valid.default <- function(data, ...){
 }
 
 #' @export
-#' @method h3_is_valid data.frame 
-h3_is_valid.data.frame <- function(data, ..., hex){
+#' @method h3_is_hex data.frame 
+h3_is_hex.data.frame <- function(data, ..., hex, .name = "is_hex"){
+  # check inputs
+  assert_that(has_it(hex))
+
+  hex_enquo <- enquo(hex)
+  hexes <- pull(data, !!hex_enquo)
+
+  data[[.name]] <- h3_is_hex(hexes)
+  return(data)
+}
+
+#' Check Pentagon
+#' 
+#' Check whether pentagons are valid.
+#' 
+#' @inheritParams h3_to_geo
+#' 
+#' @examples
+#' h3_is_pentagon("invalid")
+#' 
+#' @return A vector or original data.frame as 
+#' \link[tibble]{tibble} with an additional column 
+#' as specified by \code{.name} which defaults to 
+#' \code{is_pentagon}.
+#' 
+#' @export
+h3_is_pentagon <- function(data, ...) UseMethod("h3_is_pentagon")
+
+#' @export
+h3_is_pentagon.default <- function(data, ...){
+  map(data, function(x){
+    h3$call("h3.h3IsPentagon", x)
+  }) %>% 
+    unlist()
+}
+
+#' @export
+#' @method h3_is_pentagon data.frame 
+h3_is_pentagon.data.frame <- function(data, ..., hex, .name = "is_pentagon"){
   # check inputs
   assert_that(has_it(data))
 
   hex_enquo <- enquo(hex)
   hexes <- pull(data, !!hex_enquo)
 
-  is_valid <- map(hexes, function(x){
-    h3$call("h3.h3IsValid", x)
-  }) %>% 
-    map_dfr(function(x){
-      tibble::tibble(
-        is_valid = TRUE
-      )
-    })
+  data[[.name]] <- h3_is_pentagon(hexes)
+  return(data)
+}
 
-  bind_cols(data, is_valid)
+#' Check Class III Resolution
+#' 
+#' Whether the given H3 index is in a Class III resolution 
+#' (rotated versus the icosahedron and subject to shape 
+#' distortion adding extra points on icosahedron edges, 
+#' making them not true hexagons).
+#' 
+#' @inheritParams h3_to_geo
+#' 
+#' @examples
+#' h3_is_class_res("invalid")
+#' 
+#' @return A vector or original data.frame as 
+#' \link[tibble]{tibble} with an additional column 
+#' as specified by \code{.name} which defaults to 
+#' \code{is_pentagon}.
+#' 
+#' @export
+h3_is_class_res <- function(data, ...) UseMethod("h3_is_class_res")
+
+#' @export
+h3_is_class_res.default <- function(data, ...){
+  map(data, function(x){
+    h3$call("h3.h3IsResClassIII", x)
+  }) %>% 
+    unlist()
+}
+
+#' @export
+#' @method h3_is_class_res data.frame 
+h3_is_class_res.data.frame <- function(data, ..., hex, .name = "is_pentagon"){
+  # check inputs
+  assert_that(has_it(data))
+
+  hex_enquo <- enquo(hex)
+  hexes <- pull(data, !!hex_enquo)
+
+  data[[.name]] <- h3_is_class_res(hexes)
+  return(data)
 }
